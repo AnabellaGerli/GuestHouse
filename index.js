4c5fa57edaa7621e54fcc1e857bf67a7676257eb
Anabella Gerli <anagerli92@hotmail.com>
2020-07-13 08:18:34 +0100

    adding carousel

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ var carousel = document.getElementById("image-carousel")
 var buttonLeft = document.getElementById("button-left")
 var buttonRight = document.getElementById("button-right")
 var clicks = 0
+var viewportWidth = window.innerWidth
 
+console.log(carousel)
+console.log(buttonLeft)
+console.log(buttonRight)
 
 buttonRight.onclick = () => {
     clicks++;
-    carousel.style.left = "-100" * clicks + "px" ;
+    carousel.style.left = "-500" * clicks + "px" ;
+    if (viewportWidth > "900" && clicks >= 2)
+    {clicks = 2}
 }
 
 buttonLeft.onclick = () => {
     clicks --;
-    carousel.style.left = "-100" * clicks + "px";
+    if (viewportWidth > "900" && clicks <= 0) {
+        clicks = 0
+    }
+
+    carousel.style.left = "-500" * clicks + "px";
 
 }
-
-console.log(carousel)
